0af8da36255a94c64f91a6624d4d25a4f1135048
capoquianjojo552-dotcom <capoquianjojo552@gmail.com>
2026-03-16 18:00:12 +0800

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+function generateIDs(count) {
+  const ids = [];
+  for (let i = 0; i < count; i++) {
+   
+    if (i === 5) {
+      continue;
+    }
+    
+    ids.push(`ID-${i}`);
+  }
+  return ids;
+}
